9421fac4dae44e22af24423fab4d96103d301e7d
Chris <62134646+taarss@users.noreply.github.com>
2020-09-15 20:33:35 +0200

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -8,41 +8,43 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE PrivateCustomer(
-	[id] INT IDENTITY(1,2) PRIMARY KEY NOT NULL,
-	[firstName] NVARCHAR (100) NOT NULL,
-	[lastName] NVARCHAR (100) NOT NULL,
-	[address] NVARCHAR (100) NOT NULL,
-	[zipCode] INT NOT NULL,
-	[phoneNumber] INT NOT NULL,
+    [id] INT IDENTITY(1,2) PRIMARY KEY NOT NULL,
+    [firstName] NVARCHAR (100) NOT NULL,
+    [lastName] NVARCHAR (100) NOT NULL,
+    [address] NVARCHAR (100) NOT NULL,
+    [zipCode] INT NOT NULL,
+    [phoneNumber] INT NOT NULL,
+    [amountSpent] INT NOT NULL,
 );
 
 CREATE TABLE CorporateCustomer(
-	[id] INT IDENTITY(0,2) PRIMARY KEY NOT NULL,
-	[companyName] NVARCHAR (100) NOT NULL,
-	[seNumber] INT NOT NULL,
-	[phoneNumber] INT NOT NULL,
+    [id] INT IDENTITY(0,2) PRIMARY KEY NOT NULL,
+    [companyName] NVARCHAR (100) NOT NULL,
+    [seNumber] INT NOT NULL,
+    [phoneNumber] INT NOT NULL,
+    [amountSpent] INT NOT NULL,
 );
 
 CREATE TABLE booking(
-	[id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[workingHours] INT NOT NULL,
-	[hourlyPay] INT NOT NULL,
-	[bookingDescription] NVARCHAR (250) NOT NULL,
-	[workComplete] BIT NOT NULL,
-	[foreignKey] int NOT NULL,
-
+    [id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    [workingHours] INT NOT NULL,
+    [hourlyPay] INT NOT NULL,
+    [bookingDescription] NVARCHAR (250) NOT NULL,
+    [workComplete] BIT NOT NULL,
+    [foreignKey] int NOT NULL,
+    [date] NVARCHAR(100) NOT NULL
 );
 
 
 SET IDENTITY_INSERT [PrivateCustomer] ON
-INSERT INTO [PrivateCustomer] ([id], [firstName], [lastName], [address], [zipCode], [phoneNumber]) VALUES(1, 'Jens', 'Larsen', 'Skrænten 23', 9400, 25218414)
-INSERT INTO [PrivateCustomer] ([id], [firstName], [lastName], [address], [zipCode], [phoneNumber]) VALUES(3, 'Bent', 'Larsen', 'Skrænten 23', 9400, 25218414)
+INSERT INTO [PrivateCustomer] ([id], [firstName], [lastName], [address], [zipCode], [phoneNumber], [amountSpent]) VALUES(1, 'Jens', 'Larsen', 'SkrÃ¦nten 23', 9400, 25218414, 1000)
+INSERT INTO [PrivateCustomer] ([id], [firstName], [lastName], [address], [zipCode], [phoneNumber], [amountSpent]) VALUES(3, 'Bent', 'Larsen', 'SkrÃ¦nten 23', 9400, 25218414, 4000)
 SET IDENTITY_INSERT [PrivateCustomer] OFF
 
 
 SET IDENTITY_INSERT [CorporateCustomer] ON
-INSERT INTO [CorporateCustomer] ([id], [companyName], [seNumber], [phoneNumber]) VALUES(2, 'Jens INC', 34343434, 69696969)
-INSERT INTO [CorporateCustomer] ([id], [companyName], [seNumber], [phoneNumber]) VALUES(4, 'Lars INC', 34343434, 69696969)
+INSERT INTO [CorporateCustomer] ([id], [companyName], [seNumber], [phoneNumber], [amountSpent]) VALUES(2, 'Jens INC', 34343434, 69696969, 5500)
+INSERT INTO [CorporateCustomer] ([id], [companyName], [seNumber], [phoneNumber], [amountSpent]) VALUES(4, 'Lars INC', 34343434, 69696969, 11000)
 SET IDENTITY_INSERT [CorporateCustomer] OFF
 
 SELECT * FROM CorporateCustomer
